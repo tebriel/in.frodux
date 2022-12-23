d5f567928209ac92ac9d2a3f89b8ab5bb0240fd7
Chris Moultrie <821688+tebriel@users.noreply.github.com>
2022-12-23 14:01:33 -0500

    fix copy/pasta

--- aks.tf
+++ aks.tf
@@ -60,11 +60,11 @@ resource "azurerm_kubernetes_cluster_node_pool" "user" {
 }
 
 output "client_certificate" {
-  value     = azurerm_kubernetes_cluster.example.kube_config.0.client_certificate
+  value     = azurerm_kubernetes_cluster.aks.kube_config.0.client_certificate
   sensitive = true
 }
 
 output "kube_config" {
-  value     = azurerm_kubernetes_cluster.example.kube_config_raw
+  value     = azurerm_kubernetes_cluster.aks.kube_config_raw
   sensitive = true
 }
