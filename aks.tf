36779128be0f2c1f284cdccfcfa465ffe44e12ce
Chris Moultrie <821688+tebriel@users.noreply.github.com>
2022-12-23 13:40:04 -0500

    expand vnet and subnet size

--- aks.tf
+++ aks.tf
@@ -28,9 +28,9 @@ resource "azurerm_kubernetes_cluster" "aks" {
     type = "SystemAssigned"
   }
 
-#   aci_connector_linux {
-#     subnet_name = azurerm_subnet.virtual.name
-#   }
+  aci_connector_linux {
+    subnet_name = azurerm_subnet.virtual.name
+  }
   network_profile {
     network_plugin    = "azure"
     network_policy    = "azure"
@@ -41,8 +41,8 @@ resource "azurerm_kubernetes_cluster" "aks" {
   http_application_routing_enabled = false
 }
 
-# resource "azurerm_role_assignment" "aks-aci" {
-#   scope                = azurerm_subnet.virtual.id
-#   role_definition_name = "Network Contributor"
-#   principal_id         = azurerm_kubernetes_cluster.aks.identity.0.principal_id
-# }
+resource "azurerm_role_assignment" "aks-aci" {
+  scope                = azurerm_subnet.virtual.id
+  role_definition_name = "Network Contributor"
+  principal_id         = azurerm_kubernetes_cluster.aks.identity.0.principal_id
+}
