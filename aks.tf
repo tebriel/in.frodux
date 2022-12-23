aa5466c7ed83a6a3514d190846ee712231b44a57
Chris Moultrie <821688+tebriel@users.noreply.github.com>
2022-12-23 14:00:42 -0500

    get configs

--- aks.tf
+++ aks.tf
@@ -58,3 +58,13 @@ resource "azurerm_kubernetes_cluster_node_pool" "user" {
   max_count             = 3
   vm_size               = "Standard_B2s"
 }
+
+output "client_certificate" {
+  value     = azurerm_kubernetes_cluster.example.kube_config.0.client_certificate
+  sensitive = true
+}
+
+output "kube_config" {
+  value     = azurerm_kubernetes_cluster.example.kube_config_raw
+  sensitive = true
+}
