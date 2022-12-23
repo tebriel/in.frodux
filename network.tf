2247097d0f4152046ff0a0009a95f9c5fd1e3b6c
Chris Moultrie <821688+tebriel@users.noreply.github.com>
2022-12-23 12:44:07 -0500

    more valider?

--- network.tf
+++ network.tf
@@ -9,7 +9,7 @@ resource "azurerm_subnet" "virtual" {
   name                 = "frodux-in-virtual"
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.aks.name
-  address_prefixes     = ["10.0.0.0/24"]
+  address_prefixes     = ["10.0.1.0/24"]
 
   delegation {
     name = "aciDelegation"
@@ -24,5 +24,5 @@ resource "azurerm_subnet" "aks" {
   name                 = "frodux-in-aks"
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.aks.name
-  address_prefixes     = ["10.0.0.0/24"]
+  address_prefixes     = ["10.0.2.0/24"]
 }
