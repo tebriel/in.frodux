592ab3a00c0e8ac197d8fd8e8de765f1419d5dde
Chris Moultrie <821688+tebriel@users.noreply.github.com>
2022-12-23 12:40:37 -0500

    Just keep trying

--- network.tf
+++ network.tf
@@ -5,18 +5,11 @@ resource "azurerm_virtual_network" "aks" {
   resource_group_name = azurerm_resource_group.rg.name
 }
 
-resource "azurerm_virtual_network" "aci" {
-  name                = "frodux-in-aci"
-  address_space       = ["10.1.0.0/16"]
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-}
-
 resource "azurerm_subnet" "virtual" {
   name                 = "frodux-in-virtual"
   resource_group_name  = azurerm_resource_group.rg.name
-  virtual_network_name = azurerm_virtual_network.aci.name
-  address_prefixes     = ["10.1.0.0/24"]
+  virtual_network_name = azurerm_virtual_network.aks.name
+  address_prefixes     = ["10.0.0.0/24"]
 
   delegation {
     name = "aciDelegation"
