f7b158f13098832b53eb4354ee74a889fd835ba4
Chris Moultrie <821688+tebriel@users.noreply.github.com>
2022-12-23 12:59:06 -0500

    follow the instructions better

--- aks.tf
+++ aks.tf
@@ -14,6 +14,7 @@ resource "azurerm_kubernetes_cluster" "aks" {
     vm_size             = "Standard_B2ms"
     enable_auto_scaling = false
     os_sku              = "Ubuntu"
+    vnet_subnet_id      = azurerm_virtual_network.aks.id
   }
 
   linux_profile {
@@ -30,9 +31,9 @@ resource "azurerm_kubernetes_cluster" "aks" {
   aci_connector_linux {
     subnet_name = azurerm_subnet.virtual.name
   }
-
   network_profile {
-    network_plugin = "azure"
-    service_cidr   = "10.0.2.0/24"
+    network_plugin    = "azure"
+    network_policy    = "azure"
+    load_balancer_sku = "standard"
   }
 }
