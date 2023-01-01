32279fc44cb55aa87e2058efe073a3f0f686a458
Chris Moultrie <821688+tebriel@users.noreply.github.com>
2022-12-31 20:22:59 -0500

    subdomain validation

--- storage.tf
+++ storage.tf
@@ -9,4 +9,17 @@ resource "azurerm_storage_account" "frodux" {
     name          = "bookwyrm.static.frodux.in"
     use_subdomain = true
   }
+
+  depends_on = [
+    azurerm_dns_cname_record.storage-cname
+  ]
+}
+
+# Subdomain validation record
+resource "azurerm_dns_cname_record" "storage-cname" {
+  name                = "bookwyrm.static"
+  zone_name           = data.azurerm_dns_zone.frodux.name
+  resource_group_name = data.azurerm_resource_group.frodux-in.name
+  ttl                 = 300
+  records             = ["${azurerm_storage_account.frodux.name}.blob.core.windows.net}"]
 }
