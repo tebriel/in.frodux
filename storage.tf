5a443c788c46b5ad2c664fab48119e0afca977df
Chris Moultrie <821688+tebriel@users.noreply.github.com>
2023-01-05 16:58:05 -0500

    set cors rule on blob properties

--- storage.tf
+++ storage.tf
@@ -8,23 +8,16 @@ resource "azurerm_storage_account" "frodux" {
   account_tier                  = "Standard"
   account_replication_type      = "GRS"
   public_network_access_enabled = true
-  #   custom_domain {
-  #     name          = "bookwyrm.static.frodux.in"
-  #     use_subdomain = true
-  #   }
-}
 
-# Subdomain validation record
-resource "azurerm_dns_cname_record" "storage-cname" {
-  name                = "bookwyrm.static"
-  zone_name           = data.azurerm_dns_zone.frodux.name
-  resource_group_name = data.azurerm_resource_group.frodux-in.name
-  ttl                 = 300
-  record              = "${local.storage_account_name}.blob.core.windows.net"
-
-  depends_on = [
-    azurerm_storage_account.frodux
-  ]
+  blob_properties {
+    cors_rule {
+      allowed_headers    = ["*"]
+      allowed_methods    = ["GET", "HEAD", "POST", "PUT", "DELETE"]
+      allowed_origins    = ["https://bookwyrm.frodux.in"]
+      exposed_headers    = ["Etag"]
+      max_age_in_seconds = 3000
+    }
+  }
 }
 
 resource "azurerm_storage_container" "bookwyrm" {
